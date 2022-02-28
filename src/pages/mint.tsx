b3b2cdb6d2d2c4809d904bfa1b6280072ecde19c
feathers <sam@feathers.to>
2022-02-28 16:43:23 +1100

    Add additional states for mint page

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
+
 import { Heading, Flex, Text, Box } from '@chakra-ui/layout'
 import {
   Button,
@@ -11,6 +13,9 @@ import {
   NumberInputField,
   VStack
 } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+
+import EmailSignup from '../components/EmailSignup'
 
 import { useTranslation } from '../utils/use-translation'
 
@@ -24,8 +29,25 @@ const localisation = {
   }
 }
 
+type MintState = 'inactive' | 'active' | 'finished'
+
 const MintPage: NextPage = () => {
+  const router = useRouter()
+  const [mintState, setMintState] = useState<MintState>()
   const translate = useTranslation(localisation)
+
+  // @dev set 'mintState' based on url params
+  // @todo - update to being based on contract state
+
+  useEffect(() => {
+    setMintState(
+      router.query?.mintState === 'inactive'
+        ? 'inactive'
+        : router.query?.mintState === 'finished'
+        ? 'finished'
+        : 'active'
+    )
+  }, [router.query?.mintState])
 
   return (
     <Flex direction="row" width="100vw" height="100%">
@@ -64,7 +86,9 @@ const MintPage: NextPage = () => {
                 fontSize="20px"
                 display="inline-block"
               >
-                2 / 10000 minted
+                {mintState === 'active' && '2 / 10000 minted'}
+                {mintState === 'inactive' && 'Available from March 4th, 2022'}
+                {mintState === 'finished' && '10000 / 10000 minted'}
               </Text>
             </Box>
 
@@ -79,83 +103,115 @@ const MintPage: NextPage = () => {
               nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat.
             </Text>
-            <VStack spacing={8} align="stretch">
-              <Box maxWidth={190}>
-                <Flex
-                  w="100%"
-                  color="white"
-                  fontWeight="bold"
-                  alignItems={'center'}
-                  justifyContent="center"
-                >
-                  <NumberInput
-                    defaultValue={1}
-                    min={1}
-                    max={100}
-                    gap="8px"
-                    bg="whiteAlpha.400"
-                    p="12px"
-                    style={{ display: 'flex', borderRadius: 16 }}
+
+            {/* Minting is active */}
+            {mintState === 'active' && (
+              <VStack spacing={8} align="stretch">
+                <Box maxWidth={190}>
+                  <Flex
+                    w="100%"
+                    color="white"
+                    fontWeight="bold"
+                    alignItems="center"
+                    justifyContent="center"
                   >
-                    <NumberDecrementStepper
+                    <NumberInput
+                      defaultValue={1}
+                      min={1}
+                      max={100}
+                      alignItems="center"
+                      gap="8px"
                       bg="whiteAlpha.400"
-                      padding="4"
-                      border="none"
-                      fontSize="24px"
-                      lineHeight={'0'}
-                      width={'40px'}
-                      height={'40px'}
-                      style={{
-                        borderRadius: 12
-                      }}
+                      p="12px"
+                      style={{ display: 'flex', borderRadius: 16 }}
                     >
-                      <Text mt="-0.5">-</Text>
-                    </NumberDecrementStepper>
-                    <NumberInputField
-                      border={0}
-                      padding="2"
-                      fontWeight="bold"
-                      textAlign="center"
-                      style={{
-                        borderRadius: 12
-                      }}
-                    />
+                      <NumberDecrementStepper
+                        bg="whiteAlpha.400"
+                        padding="4"
+                        border="none"
+                        fontSize="24px"
+                        lineHeight={'0'}
+                        width={'40px'}
+                        height={'40px'}
+                        style={{
+                          borderRadius: 12
+                        }}
+                      >
+                        <Text mt="-0.5">-</Text>
+                      </NumberDecrementStepper>
+                      <NumberInputField
+                        border={0}
+                        padding="2"
+                        fontWeight="bold"
+                        textAlign="center"
+                        style={{
+                          borderRadius: 12
+                        }}
+                      />
 
-                    <NumberIncrementStepper
-                      bg="whiteAlpha.400"
-                      padding="4"
-                      border="none"
-                      fontSize="24px"
-                      width={'40px'}
-                      height={'40px'}
-                      style={{
-                        borderRadius: 12
-                      }}
-                    >
-                      <Text mt="-0.5">+</Text>
-                    </NumberIncrementStepper>
-                  </NumberInput>
+                      <NumberIncrementStepper
+                        bg="whiteAlpha.400"
+                        padding="4"
+                        border="none"
+                        fontSize="24px"
+                        width={'40px'}
+                        height={'40px'}
+                        style={{
+                          borderRadius: 12
+                        }}
+                      >
+                        <Text mt="-0.5">+</Text>
+                      </NumberIncrementStepper>
+                    </NumberInput>
+                  </Flex>
+                </Box>
+
+                <Flex alignItems={'center'} gap="8">
+                  <Button
+                    onClick={() => alert('Mint')}
+                    fontSize="24px"
+                    py={6}
+                    colorScheme="yellow"
+                    style={{
+                      boxShadow: '0 0 0 8px rgba(255, 213, 0, 0.2)',
+                      borderRadius: '12px'
+                    }}
+                  >
+                    {translate('mintButton')}
+                  </Button>
+                  <Text fontSize="32px" fontWeight="bold">
+                    12 ETH
+                  </Text>
                 </Flex>
-              </Box>
+              </VStack>
+            )}
 
-              <Flex alignItems={'center'} gap="8">
+            {/* Minting has not started yet */}
+            {mintState === 'inactive' && <EmailSignup />}
+
+            {/* Minting has finished */}
+            {mintState === 'finished' && (
+              <Box>
                 <Button
-                  onClick={() => alert('Mint')}
-                  fontSize="24px"
-                  py={6}
-                  colorScheme="yellow"
-                  style={{
-                    boxShadow: '0 0 0 8px rgba(255, 213, 0, 0.2)',
-                    borderRadius: '12px'
-                  }}
+                  as="a"
+                  href="https://opensea.io/"
+                  target="_blank"
+                  textColor="black"
+                  bg="white"
+                  rounded="xl"
+                  rightIcon={
+                    <Image
+                      height={24}
+                      width={26}
+                      src="/opensea-ship.svg"
+                      alt="ETH Logo"
+                    />
+                  }
                 >
-                  {translate('mintButton')}
+                  Browse on Opensea
                 </Button>
-                <Text fontSize="32px" fontWeight="bold">
-                  12 ETH
-                </Text>
-              </Flex>
-            </VStack>
+              </Box>
+            )}
           </VStack>
         </GridItem>
       </Grid>
