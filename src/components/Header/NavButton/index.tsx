31ccad1597f41a15cedba2f9cfd7087c4079b14a
C. Lewis <ctj.lewis@icloud.com>
2022-02-27 23:04:38 -0600

    chore: use Tailwind grid, flex + gap over padding

--- src/components/Header/NavButton/index.tsx
+++ src/components/Header/NavButton/index.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 export const NavButton: FC<ButtonProps> = ({ children }) => {
   return (
     <Button
-      className="nav-button"
+      className="nav-button flex flex-row gap-2"
       backgroundColor="#FFD500"
       color="black"
       borderRadius="25px"
