e21fe88b942cd72d1fc20f52526220e084c6583d
feathers <sam@feathers.to>
2022-02-28 22:04:34 +1100

    Update mint page designs

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -2,28 +2,21 @@ import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 
 import { Heading, Flex, Text, Box } from '@chakra-ui/layout'
-import {
-  Button,
-  Image,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  VStack
-} from '@chakra-ui/react'
+import { Button, Image, VStack } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-
-import EmailSignup from '../components/EmailSignup'
+import { InputNumber } from '../components/InputNumber'
 
 import { useTranslation } from '../utils/use-translation'
 
 const localisation = {
   en: {
-    mintButton: 'Mint NFT'
+    mintButton: 'Mint NFT',
+    connectWallet: 'Connect Wallet'
   },
   fr: {
     // @todo translate
-    mintButton: 'Mint NFT'
+    mintButton: 'Mint NFT',
+    connectWallet: 'Connect Wallet'
   }
 }
 
@@ -32,6 +25,8 @@ type MintState = 'inactive' | 'active' | 'finished'
 const MintPage: NextPage = () => {
   const router = useRouter()
   const [mintState, setMintState] = useState<MintState>()
+  const [walletConnected, setWalletConnected] = useState(true)
+  const [mintCount, setMintCount] = useState(1)
   const translate = useTranslation(localisation)
 
   // @dev set 'mintState' based on url params
@@ -48,31 +43,31 @@ const MintPage: NextPage = () => {
   }, [router.query?.mintState])
 
   return (
-    <Flex direction="row" width="100%" height="100%" px={8} py={8}>
-      <div className="max-w-7xl mx-auto">
-        <div className="grid gap-8 grid-cols-12">
-          <div className="col-span-12 md:col-span-5">
+    <Flex direction="row" width="100%" height="100%">
+      <div className="max-w-7xl mx-auto p-8">
+        <div className="grid md:gap-8 md:grid-cols-12">
+          <div className="md:col-span-5 mb-8">
             <Box rounded="3xl" bg="white" overflow={'hidden'}>
-              <Image src="/example-nft.png" alt="Example NFT" />
+              <Image src="/example-nft-2.png" alt="Example NFT" />
               <Flex justifyContent={'space-between'} p="4">
                 <Text textColor="black" fontWeight={'bold'}>
-                  NFT Name
+                  Bubki NFTs
                 </Text>
                 <Text textColor="black" fontWeight={'bold'}>
-                  2 ETH
+                  0.05 ETH each
                 </Text>
               </Flex>
             </Box>
           </div>
-          <div className="col-span-12 md:col-start-7 md:col-span-5">
+          <div className="md:col-start-7 md:col-span-5">
             <VStack spacing={6} align="stretch">
               <Box>
                 <Text
                   as="span"
                   px={4}
-                  py={2}
+                  py="5px"
                   bg="whiteAlpha.400"
-                  rounded="lg"
+                  rounded="xl"
                   fontWeight="semibold"
                   fontSize="20px"
                   display="inline-block"
@@ -84,7 +79,7 @@ const MintPage: NextPage = () => {
               </Box>
 
               <Heading fontSize={48} lineHeight={1.33}>
-                NFT Name Goes Here
+                Bubki NFTs
               </Heading>
 
               <Text as="p" fontSize="20px">
@@ -98,70 +93,19 @@ const MintPage: NextPage = () => {
               {/* Minting is active */}
               {mintState === 'active' && (
                 <VStack spacing={8} align="stretch">
-                  <Box maxWidth={190}>
-                    <Flex
-                      w="100%"
-                      color="white"
-                      fontWeight="bold"
-                      alignItems="center"
-                      justifyContent="center"
-                    >
-                      <NumberInput
-                        defaultValue={1}
-                        min={1}
-                        max={100}
-                        alignItems="center"
-                        gap="8px"
-                        bg="whiteAlpha.400"
-                        p="12px"
-                        style={{ display: 'flex', borderRadius: 16 }}
-                      >
-                        <NumberDecrementStepper
-                          bg="whiteAlpha.400"
-                          padding="4"
-                          border="none"
-                          fontSize="24px"
-                          lineHeight={'0'}
-                          width={'40px'}
-                          height={'40px'}
-                          style={{
-                            borderRadius: 12
-                          }}
-                        >
-                          <Text mt="-0.5">-</Text>
-                        </NumberDecrementStepper>
-                        <NumberInputField
-                          border={0}
-                          padding="2"
-                          fontWeight="bold"
-                          textAlign="center"
-                          style={{
-                            borderRadius: 12
-                          }}
-                        />
+                  <Flex alignItems={'center'} gap="6">
+                    <InputNumber onChange={(value) => setMintCount(value)} />
+                    <Text fontSize="24px" fontWeight="bold">
+                      {Math.round(mintCount * 0.05 * 100) / 100} ETH
+                    </Text>
+                  </Flex>
 
-                        <NumberIncrementStepper
-                          bg="whiteAlpha.400"
-                          padding="4"
-                          border="none"
-                          fontSize="24px"
-                          width={'40px'}
-                          height={'40px'}
-                          style={{
-                            borderRadius: 12
-                          }}
-                        >
-                          <Text mt="-0.5">+</Text>
-                        </NumberIncrementStepper>
-                      </NumberInput>
-                    </Flex>
-                  </Box>
-
-                  <Flex alignItems={'center'} gap="8">
+                  <VStack spacing={4} align="stretch" maxWidth="320px">
                     <Button
+                      width="100%"
                       onClick={() => alert('Mint')}
                       fontSize="24px"
-                      py={6}
+                      py="27px"
                       colorScheme="yellow"
                       style={{
                         boxShadow: '0 0 0 8px rgba(255, 213, 0, 0.2)',
@@ -170,15 +114,46 @@ const MintPage: NextPage = () => {
                     >
                       {translate('mintButton')}
                     </Button>
-                    <Text fontSize="32px" fontWeight="bold">
-                      12 ETH
-                    </Text>
-                  </Flex>
+                    <Text textAlign="center">Max 20 per transaction</Text>
+                  </VStack>
                 </VStack>
               )}
 
-              {/* Minting has not started yet */}
-              {mintState === 'inactive' && <EmailSignup />}
+              {/* Minting has not started yet & wallet not connected */}
+              {mintState === 'inactive' && !walletConnected && (
+                <VStack spacing={4} align="stretch" maxWidth="300px">
+                  <Button
+                    width="100%"
+                    // @todo - connect wallet
+                    onClick={() => alert('Connect Wallet')}
+                    fontSize="24px"
+                    py={6}
+                    colorScheme="yellow"
+                    style={{
+                      boxShadow: '0 0 0 8px rgba(255, 213, 0, 0.2)',
+                      borderRadius: '12px'
+                    }}
+                  >
+                    {translate('connectWallet')}
+                  </Button>
+                </VStack>
+              )}
+
+              {/* Minting has not started yet & wallet connected */}
+              {mintState === 'inactive' && walletConnected && (
+                <Box>
+                  <Button
+                    as="a"
+                    href="https://twitter.com/"
+                    target="_blank"
+                    textColor="black"
+                    bg="white"
+                    rounded="xl"
+                  >
+                    Follow @TwitterHandle
+                  </Button>
+                </Box>
+              )}
 
               {/* Minting has finished */}
               {mintState === 'finished' && (
@@ -194,7 +169,7 @@ const MintPage: NextPage = () => {
                       <Image
                         height={24}
                         width={26}
-                        src="/opensea-ship.svg"
+                        src="/icons/opensea-ship.svg"
                         alt="ETH Logo"
                       />
                     }
