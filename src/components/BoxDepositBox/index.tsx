067930c1c5ed4c61d5864a45941b22cc495bb671
garrettjmu <garretthughes3@gmail.com>
2022-03-01 02:17:36 -0500

    make it editable

--- src/components/BoxDepositBox/index.tsx
+++ src/components/BoxDepositBox/index.tsx
@@ -1,10 +1,14 @@
 import { Box, Text } from '@chakra-ui/layout'
+import { Input } from '@chakra-ui/react'
 import AssetMenu from '../AssetMenu'
 import { useEthers, useTokenBalance } from '@usedapp/core'
 import { usdcTokenAddress } from '../../utils/poolTogether'
 import { utils } from 'ethers'
+import React, { useState } from 'react'
 
 const BoxDepositBox = () => {
+  const [amountToDonate, setAmountToDonate] = useState(0)
+
   const { account } = useEthers()
   const tokenBalance = useTokenBalance(usdcTokenAddress, account)
 
@@ -23,9 +27,15 @@ const BoxDepositBox = () => {
       justifyContent="space-between"
     >
       <Box color="white">
-        <Text fontSize="36px" fontWeight="bold">
-          5
-        </Text>
+        <Input
+          fontSize="36px"
+          fontWeight="bold"
+          border="none"
+          focusBorderColor="none"
+          value={amountToDonate}
+          onChange={(e: React.ChangeEvent) => setAmountToDonate(e.target.value)}
+          pl={0}
+        />
         <Text color="#DADADA">
           Balance: {tokenBalance ? utils.formatEther(tokenBalance) : 0.0}
         </Text>
