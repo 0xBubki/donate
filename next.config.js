2cd6a9c1159e1618b0e73000ba49f64353c982d7
Justin Michaliga <justin.michaliga@gmail.com>
2022-03-02 15:46:14 -0500

    added favicons and translations boilerplate

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['de', 'en', 'es', 'fr']
+    locales: ['de', 'en', 'es', 'fr', 'ua']
   },
   images: {
     domains: ['cdn.jsdelivr.net']
