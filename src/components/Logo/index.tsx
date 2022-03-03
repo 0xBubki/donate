1230bf04a2c650863d9d8865dd8fc9f1d83b90fb
C. Lewis <ctj.lewis@icloud.com>
2022-03-02 23:09:49 -0600

    chore: use sunflower

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   return (
     <NextLink href="/" passHref>
       <div className="center no-underline cursor-pointer select-none font-bold p-2">
-        <span className="p-0 xs:p-2">🇺🇦</span>
+        <span className="p-0 xs:p-2">🌻</span>
         <span className="hidden xs:block text-sm xs:text-md md:text-xl">
           Bubki
         </span>
